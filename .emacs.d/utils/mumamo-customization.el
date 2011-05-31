b1bccec6130a17034205e93ea25f74056c0fa3f6
Rotem Yaari <vmalloc@gmail.com>
2011-05-31 11:23:03 +0300

    Make colors on mumamo-mode saner

---
+++ .emacs.d/utils/mumamo-customization.el
@@ -0,0 +1,12 @@
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "apple" :family "Monaco"))))
+ '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) (:background "black"))))
+ '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) (:background "Midnight"))))
+ '(mumamo-background-chunk-submode2 ((((class color) (min-colors 88) (background dark)) (:background "#002200"))))
+ '(mumamo-background-chunk-submode3 ((((class color) (min-colors 88) (background dark)) (:background "#550000")))))
+
+(provide 'mumamo-customization)
