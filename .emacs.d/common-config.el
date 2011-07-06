38dc6b331f4fd8068ec5b55b5d4d3f485a8fd372
Rotem Yaari <vmalloc@gmail.com>
2011-07-06 16:37:02 +0300

    C/C++ compilation improvements

--- .emacs.d/common-config.el
+++ .emacs.d/common-config.el
@@ -156,6 +156,23 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands) 
 
 ;------------------------------------------------------------------------------;
+; C/C++
+;------------------------------------------------------------------------------;
+(setq compilation-window-height 8)
+(setq compilation-finish-function
+      (lambda (buf str)
+
+        (if (string-match "exited abnormally" str)
+
+            ;;there were errors
+            (message "compilation errors, press C-x ` to visit")
+
+          ;;no errors, make the compilation window go away in 0.5 seconds
+          (run-at-time 0.5 nil 'delete-windows-on buf)
+          (message "NO COMPILATION ERRORS!"))))
+    
+
+;------------------------------------------------------------------------------;
 ; Customization                                                                ;
 ;------------------------------------------------------------------------------;
 (require 'customization)
