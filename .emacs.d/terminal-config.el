5e85379e0953c62db5cd6180ea73ad6e7f9cb6c4
Rotem Yaari <vmalloc@gmail.com>
2011-07-31 09:41:45 +0300

    Terminal to properly map Ctrl-Backspace in Mac

--- .emacs.d/terminal-config.el
+++ .emacs.d/terminal-config.el
@@ -5,3 +5,8 @@
 ;------------------------------------------------------------------------------;
 (define-key input-decode-map "\e[1;2A" [S-up])
 
+;------------------------------------------------------------------------------;
+; Control-backspace hack for Mac terminals. You need to make your term send    ;
+; this sequence in order for this to work                                      ;
+;------------------------------------------------------------------------------;
+(define-key input-decode-map "\e?" [C-backspace])
