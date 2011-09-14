9fced11b996f2c5f7240bd49ac37f46e78e1dcd8
Rotem Yaari <vmalloc@gmail.com>
2011-09-14 10:23:40 +0300

    Don't close compilation window if it's a grep result

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -165,15 +165,15 @@
 (setq compilation-window-height 8)
 (setq compilation-finish-function
       (lambda (buf str)
+        (if (not (string-match "grep" (buffer-name buf)))
+            (if (string-match "exited abnormally" str)
 
-        (if (string-match "exited abnormally" str)
-
-            ;;there were errors
-            (message "compilation errors, press C-x ` to visit")
-
-          ;;no errors, make the compilation window go away in 0.5 seconds
-          (run-at-time 0.5 nil 'delete-windows-on buf)
-          (message "NO COMPILATION ERRORS!"))))
+                ;;there were errors
+                (message "compilation errors, press C-x ` to visit")
+              
+              ;;no errors, make the compilation window go away in 0.5 seconds
+              (run-at-time 0.5 nil 'delete-windows-on buf)
+              (message "NO COMPILATION ERRORS!")))))
 (require 'xcscope)
 
 
